4346985c832087af0d73e137330e4990a1391770
ian <mello.ian@hotmail.com>
2022-07-28 13:50:35 -0300

    all

---
+++ MD5_HASH/Hashing.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+namespace MD5_HASH
+{
+    public class Hashing
+    {
+        public static string ToMD5(string value)
+        {
+            using var md5 = MD5.Create();
+            byte[] bytes = md5.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+            var sb = new StringBuilder();
+            for(int i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+        public static string ToSHA256(string value)
+        {
+            using var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+            var sb = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                sb.Append(bytes[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
